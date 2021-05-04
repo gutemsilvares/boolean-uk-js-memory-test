f1c10d163a9eb9d5b756b46cd8ae0a379823e5ef
Agutembergues <gutemsilvares@hotmail.com>
2021-05-04 23:15:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// SetInterval
+
+const numbers = [20, 40, 60, 100];
+const result = numbers;
+
+setTimeout(function () {
+	alert("Remember this Numbers: " + numbers); //set once 5 seconds
+}, 5000);
+
+prompt("Put the Numbers: ");
+// setTimeout(function () {
+
+// }, 2000);
+
+if (prompt === result) {
+	alert("The number is in order");
+} else {
+	alert("The numbers is not in order");
+}
+
